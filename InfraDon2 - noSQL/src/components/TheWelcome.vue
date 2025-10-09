e3b97647f82b29f09398a1972e7b2a78b94b3f47
Lea Pires <lea.pires@heig-vd.ch>
2025-10-09 12:36:28 +0200

    added mounted function

--- InfraDon2 - noSQL/src/components/TheWelcome.vue
+++ InfraDon2 - noSQL/src/components/TheWelcome.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
  
 import { ref } from 'vue'
-import PouchDB from 'pouchdb
+import PouchDB from 'pouchdb'
 //DATA VARIABLES
 const counter = ref(0);
  
@@ -10,11 +10,9 @@ const increment = () => {
   counter.value++
 }
 
-const db = new PouchDB(http)
-
- 
-</script>
-
 mounted() {
     this.initDatabase()
 }
+
+</script>
+
