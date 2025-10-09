a2fc92283e1390540d57e0f988349ef5c10f6cfb
Lea Pires <lea.pires@heig-vd.ch>
2025-10-09 11:30:32 +0200

    add data/data.js and install pouchdb

---
+++ InfraDon2 - noSQL/src/components/TheWelcome.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+ 
+import { ref } from 'vue'
+//DATA VARIABLES
+const counter = ref(0);
+ 
+//FUNCTIONS / METHODS -> CONTROLLER
+const increment = () => {
+  counter.value++
+}
+ 
+</script>
